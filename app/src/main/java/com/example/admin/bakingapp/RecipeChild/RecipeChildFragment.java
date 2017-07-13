23b780e69fa1152f54eac0b3b675f7ebb0fd65ac
Tony <thanhlong.le2002@gmail.com>
2017-07-13 15:10:43 +0700

    Added fragments
    Tried adding master detail flow

--- app/src/main/java/com/example/admin/bakingapp/RecipeChild/RecipeChildFragment.java
+++ app/src/main/java/com/example/admin/bakingapp/RecipeChild/RecipeChildFragment.java
@@ -7,13 +7,11 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.example.admin.bakingapp.NetworkUtils;
 import com.example.admin.bakingapp.R;
